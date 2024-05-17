019f8abbd0bd0240395fe19e23e458d9fd8866eb
Cristian Giraldo <cgiraldovi@hotmail.com>
2024-05-17 18:36:43 -0500

    Update Serie.jsx

--- src/MediaContent/Pages/Serie.jsx
+++ src/MediaContent/Pages/Serie.jsx
@@ -1,23 +1,5 @@
-import { useEffect, useState } from "react";
 
 export const Serie = () => {
-
-  const [token, setToken] = useState(null);
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    if (storedToken) {
-      setToken(storedToken);
-      console.log(storedToken);
-    } else {
-      console.log("No JWT found");
-    }
-
-    const decodedToken = JSON.parse(atob(token.split('.')[1]));
-    console.log(decodedToken);
-
-
-  }, []);
 
   return (
     <>
